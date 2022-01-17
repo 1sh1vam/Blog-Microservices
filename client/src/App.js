c82bbefaf03fb44629550034603fc7c02c7c8112
1sh1vam <shivam07.mt@gmail.com>
2022-01-17 21:00:50 +0530

    adding the client react project

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CreatePost from './CreatePost';
+import PostsList from './PostsList';
+
+export default function App() {
+    return (
+        <div className="container">
+            <h1>Create Post</h1>
+            <CreatePost />
+            <hr />
+            <h1>Posts</h1>
+            <PostsList />
+        </div>
+    )
+};
