162b802ed5bc02038f76591c7b499003b7bf1307
1sh1vam <shivam07.mt@gmail.com>
2022-01-17 20:59:08 +0530

    added the comments service

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    const { id } = req.params;
+    res.send(commentsByPostId[id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const postId = req.params.id;
+    const { content } = req.body;
+
+    const id = randomBytes(4).toString('hex');
+    const comments = commentsByPostId[postId] || [];
+    comments.push({id, content});
+
+    commentsByPostId[postId] = comments;
+
+    res.status(201).send(comments)
+});
+
+app.listen(4001, () => {
+    console.log('Listening on port 4001..');
+});
