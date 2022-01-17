ff797aa2b87e0f2353996204a530ef87d42cf124
1sh1vam <shivam07.mt@gmail.com>
2022-01-17 21:02:19 +0530

    CreatePost component

---
+++ client/src/CreatePost.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function CreatePost() {
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await axios.post('http://localhost:4000/posts', {title});
+        setTitle('');
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group" style={{marginBottom: '20px'}}>
+                    <label>Title</label>
+                    <input value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
+};
