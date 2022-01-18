b161c18078a10a464f118a95bfd87ee5a7f86d94
1sh1vam <shivam07.mt@gmail.com>
2022-01-18 20:11:27 +0530

    modifying the react app to use new query service to fetch data

--- client/src/PostsList.js
+++ client/src/PostsList.js
@@ -7,7 +7,7 @@ export default function PostsList() {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        const res = await axios.get('http://localhost:4002/posts');
         setPosts(res.data);
     };
 
@@ -23,7 +23,7 @@ export default function PostsList() {
         >
             <div className="card-body">
                 <h3>{post.title}</h3>
-                <CommentsList postId={post.id} />
+                <CommentsList comments={post.comments} />
                 <CreateComment postId={post.id} />
             </div>
         </div>
