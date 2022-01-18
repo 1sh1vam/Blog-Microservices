a2ace43ca1d04648c2530377d69ca294df3ac9ea
1sh1vam <shivam07.mt@gmail.com>
2022-01-18 19:58:08 +0530

    Parsing the events in Query service

--- query/index.js
+++ query/index.js
@@ -5,12 +5,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
+const posts = {};
 
+app.get('/posts', (req, res) => {
+    res.send(posts);
 });
 
 app.post('/events', (req, res) => {
-    const { type, pos }
+    const { type, data } = req.body;
+
+    if (type === 'PostCreated') {
+        const { id } = data;
+        posts[id] = { ...data, comments: [] };
+    };
+
+    if (type === 'CommentCreated') {
+        const { postId, id, content } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
 });
 
 app.listen(4002, () => {
