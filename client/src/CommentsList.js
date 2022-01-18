78fe41621ff4926d4abeee43f39bb9edad47ce76
1sh1vam <shivam07.mt@gmail.com>
2022-01-18 20:12:13 +0530

    removing unnecessary dependency from the CommentsList component

--- client/src/CommentsList.js
+++ client/src/CommentsList.js
@@ -1,6 +1,3 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-
 export default function CommentsList({ comments }) {
     const renderedComments = comments.map((comment) => (
         <li key={comment.id}>{comment.content}</li>
