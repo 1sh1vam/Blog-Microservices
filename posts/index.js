90b37c97abcd092eda52c9be7c85b6445ec585d6
1sh1vam <shivam07.mt@gmail.com>
2022-01-18 19:39:48 +0530

    listening for the events in posts service

--- posts/index.js
+++ posts/index.js
@@ -19,6 +19,11 @@ app.post('/posts', (req, res) => {
     res.status(201).send(posts[id]);
 });
 
+app.post('/events', (req, res) => {
+    console.log('Received Events', req.body.type);
+    res.send({});
+});
+
 app.listen(4000, () => {
     console.log('listening on port 4000...');
 });
