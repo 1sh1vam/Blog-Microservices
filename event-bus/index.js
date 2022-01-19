ac3cf4a0cec93bec3527690db7b730c8ed41caac
1sh1vam <shivam07.mt@gmail.com>
2022-01-19 19:01:09 +0530

    added the moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 app.post('/events', (req, res) => {
     const event = req.body;
