b7d5a78625da59a87597d91a1e7c96d3a65276e6
1sh1vam <shivam07.mt@gmail.com>
2022-01-19 19:24:22 +0530

    listening for the CommentCreated event and moderating the comment in moderation service

---
+++ moderation/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const { id, postId, content } = data
+        const status = content.includes('orange') ? 'rejected' : 'approved';
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: { ...data, status, content }
+        });
+    };
+
+    res.send({})
+});
+
+app.listen(4003, () => {
+    console.log('listening at port 4003');
+});
