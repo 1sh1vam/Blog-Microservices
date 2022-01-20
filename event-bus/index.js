771cf807eea029bc6a14b7cd87c17f4716446d49
1sh1vam <shivam07.mt@gmail.com>
2022-01-20 19:58:26 +0530

    handling the error

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-const evetnts = [];
+const events = [];
 
 app.post('/events', (req, res) => {
     const event = req.body;
@@ -13,8 +13,8 @@ app.post('/events', (req, res) => {
 
     axios.post('http://localhost:4000/events', event);
     axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    axios.post('http://localhost:4003/events', event);
+    axios.post('http://localhost:4002/events', event).catch((e) => console.log('error', e));
+    axios.post('http://localhost:4003/events', event).catch((e) => console.log('error', e));
 
     res.send({ status: 'OK'});
 });
