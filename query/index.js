915baa08c5173a19d953e626471af6e3c6f5d127
1sh1vam <shivam07.mt@gmail.com>
2022-01-20 19:59:00 +0530

    receiving all the events after query service is up again

--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -7,14 +8,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-    console.log('Received event ', type);
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id } = data;
         posts[id] = { ...data, comments: [] };
@@ -33,8 +27,26 @@ app.post('/events', (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
 });
 
-app.listen(4002, () => {
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    console.log('Received event ', type);
+    handleEvent(type, data);
+});
+
+app.listen(4002, async () => {
     console.log('listening on port 4002');
+
+    const res = await axios.get('http://localhost:4005/events');
+
+    for(let event of res.data) {
+        const { type, data } = event;
+        console.log('Received event ,', type);
+        handleEvent(type, data);
+    }
 });
